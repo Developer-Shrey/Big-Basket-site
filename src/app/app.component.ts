0a0516bf48ed688f91b87d6d4ea3a0e5d87d161b
Developer-Shrey <shreyash.manapure97@gmail.com>
2023-09-05 20:04:15 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+// import {faSearch, faBagShopping,faAddressBook,faMapPin} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'big-basket';
+
+}
+
+
